ef268ec5c8ed51a4e3a2acd474b59f5e51241c84
Laura <laura.reivinen@gmail.com>
2019-08-20 13:02:42 +0300

    kirjautuminen lisätty, ja genrejen rajaus, 8.18-8.19

--- Library/library-frontend/src/App.js
+++ Library/library-frontend/src/App.js
@@ -2,20 +2,12 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 import { Query, Mutation } from 'react-apollo'
 import { gql } from 'apollo-boost'
+import { useMutation, useApolloClient } from '@apollo/react-hooks'
 
-const App = () => {
-  const [page, setPage] = useState('authors')
-  const [errorMessage, setErrorMessage] = useState(null)
-  const handleError = (error) => {
-    setErrorMessage(error.graphQLErrors[0].message)
-    setTimeout(() => {
-      setErrorMessage(null)
-    }, 10000)
-  }
-
-  const ALL_AUTHORS= gql`
+const ALL_AUTHORS= gql`
   {
     allAuthors  {
       name
@@ -26,7 +18,7 @@ const App = () => {
   }
   `
 
-  const ALL_BOOKS = gql`
+const ALL_BOOKS = gql`
   {
     allBooks  {
       title
@@ -36,29 +28,79 @@ const App = () => {
       }
       published
       id
+      genres
     }
   }
   `
-  const CREATE_BOOK = gql`
-  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
-    addBook(
-      title: $title,
-      author:  $author,
-      published: $published,
-      genres: $genres,
-    ) {
-      title
-      author {
-        name
-        id
-      }
-      published
-      genres
+const CREATE_BOOK = gql`
+mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+  addBook(
+    title: $title,
+    author:  $author,
+    published: $published,
+    genres: $genres,
+  ) {
+    title
+    author {
+      name
       id
     }
+    published
+    genres
+    id
   }
-  `
+}
+`
 
+const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password)  {
+      value
+    }
+  }
+`
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const handleError = (error) => {
+    setErrorMessage(error.graphQLErrors[0].message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
+  const [login] = useMutation(LOGIN, {
+    onError: handleError
+  })
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  const errorNotification = () => 
+    errorMessage &&
+    <div style={{ color: 'red' }}>
+      {errorMessage}
+    </div>
+
+  if (!token) {
+    return (
+      <div>
+        {errorNotification()}
+        <h2>Login</h2>
+        <LoginForm
+          login={login}
+          setToken={(token) => setToken(token)}
+        />
+      </div>
+    )
+  }
   
 
   return (
@@ -72,6 +114,10 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        { (!token) ? 
+          <button onClick={() => setPage('login')}>login</button> 
+          :
+          <button onClick={logout}>logout</button> }
       </div>
 
       <Query query={ALL_AUTHORS} pollInterval={2000} >
@@ -84,7 +130,7 @@ const App = () => {
         }
       </Query>
 
-      <Query query={ALL_BOOKS} pollInterval={2000}>
+      <Query query={ALL_BOOKS} >
         {(result) => <Books
           result={result}
           show={page === 'books'}
--- Library/library-frontend/src/components/Books.js
+++ Library/library-frontend/src/components/Books.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Books = (props) => {
+  const [ showAll, setShowAll] = useState(true)
+  const [ genre, setGenre ] = useState('')
+
   if (!props.show) {
     return null
   }
   const books = props.result.data.allBooks
+  const findGenres = () => {
+    let genres = []
+    books.map(b => b.genres.map(genre => {
+    if (!genres.includes(genre)) {
+      genres = genres.concat(genre)
+    }
+  }))
+    return genres
+  }
+
+  const handleClick = (e) => {
+    console.log('value ', e.target.value)
+    if (e.target.value === 'all') {
+      setGenre('')
+      setShowAll(true)
+    } else {
+      setShowAll(false)
+      setGenre(e.target.value)
+    }
+  }
 
   return (
     <div>
@@ -21,15 +44,31 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {showAll ? 
+          books.map(book =>
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
             </tr>
-          )}
+          ) : 
+            books.map(book =>  {
+              if (book.genres.includes(genre)) {
+                return (
+                  <tr key={book.title}>
+                    <td>{book.title}</td>
+                    <td>{book.author.name}</td>
+                    <td>{book.published}</td>
+                  </tr>
+                )
+              }
+            }
+            )
+          }
         </tbody>
       </table>
+    {findGenres().map(g => <button key={g} type='submit' value={g} onClick={ handleClick }>{g}</button>)}
+    <button key="all" id="findAll" type="submit" value='all' onClick={ handleClick }>all genres</button>
     </div>
   )
 }
